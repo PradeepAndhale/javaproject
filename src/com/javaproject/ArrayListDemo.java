95660ae1b67f5b6ad15a3758dad40abb1cd9aca7
PradeepAndahle <andhalepradeep77@gmail.com>
2022-12-24 20:37:51 +0530

    first commit : added arraylist demo code

---
+++ src/com/javaproject/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.javaproject;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> arrayList=new ArrayList<String>();
+		
+		arrayList.add("Ram");
+		arrayList.add("Shyam");
+		arrayList.add("Dhiraj");
+		
+		System.out.println(arrayList);
+	}
+}
